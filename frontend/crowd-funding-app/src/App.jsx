5d3e10d23debb4c1a1a2eeae1b0d8609fff95eba
Yohannes <54820434+yohannesabebe@users.noreply.github.com>
2025-03-11 22:10:17 +0300

    Added Token Swap component. Background Color Changed.

--- frontend/crowd-funding-app/src/App.jsx
+++ frontend/crowd-funding-app/src/App.jsx
@@ -5,12 +5,13 @@ import WagasOffering from "./sections/WagasOffering";
 import Footer from "./components/Footer";
 import Roadmap from "./components/Roadmap";
 import Faq from "./components/Faq";
-
+import OurPartners from "./components/OurPartners";
 function App() {
   return (
     <div className="min-h-screen">
       <Nav />
       <Hero />
+      <OurPartners />
       <Roadmap />
       <HowToBuy />
       <WagasOffering />
